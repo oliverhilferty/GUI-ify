56fbecff576b5b364790bc9ee237ec39c54a8fc8
Oliver <oliver.hilferty@rezonence.com>
2019-04-12 10:04:38 +0100

    Add functions to parse outputted help text

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ const execSync = require('child_process').execSync;
 const exec = require('child_process').exec;
 const chalk = require('chalk');
 
+/**
+ * Get the string of arguments from a CLI's help string
+ * @param {String} helpOutput
+ * @returns {number | string}
+ */
+let getUsageString = (helpOutput) => {
+    let usageLines = helpOutput.split('\n');
+    for (let line of usageLines) {
+        if (line.includes('usage')) {
+            return line;
+        }
+    }
+    return -1;
+};
+
+let parseUsageString = (usageString) => {
+    return usageString.split(/\[|] \[|] /).filter(el => el !== '');
+};
+
 let parser = new ArgumentParser({
     addHelp: true,
     prog: "GUI-ify"
@@ -17,5 +36,9 @@ parser.addArgument(
 
 let args = parser.parseArgs();
 
-out = execSync(`${args.CLI} --help`).toString();
-console.log(out);
+let out = execSync(`${args.CLI} --help`).toString();
+let usageString = getUsageString(out);
+let params = parseUsageString(usageString);
+
+console.log(params);
+
