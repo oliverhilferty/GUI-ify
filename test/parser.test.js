ffd342175d471ceeaa24f3509488bdb53caa5d22
oliverhilferty <oliverhilferty@gmail.com>
2019-05-23 12:02:29 +0100

    Set up Mocha and Chai, and the describe block

---
+++ test/parser.test.js
@@ -0,0 +1,9 @@
+const describe = require('mocha').describe;
+const chai = require('chai');
+chai.should();
+
+describe('Parser', function() {
+    it('Should run this test', function() {
+
+    });
+});
