efffbe24d0ee955e3038a4f982e19bba6808e16b
oliverhilferty <oliverhilferty@gmail.com>
2019-05-23 18:08:26 +0100

    Move string to variable

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,6 +2,7 @@ const describe = require('mocha').describe;
 require('chai').should();
 const CommandLineHelpParser = require('../parser').CommandLineHelpParser;
 
+const usageString = 'usage: grablio [-h] [-d DESTINATION] [-s SELECTOR] [-a ATTR] [-t] url';
 
 describe('Parser', function() {
     beforeEach('Init CommandLineHelpParser', function() {
@@ -9,9 +10,9 @@ describe('Parser', function() {
     });
 
     it('generates an object with optional arguments, positional arguments, and flags fields', function() {
-        const parsedString = this.CLIParser.parse('usage: grablio [-h] [-d DESTINATION] [-s SELECTOR] [-a ATTR] [-t] url');
+        const parsedString = this.CLIParser.parse(usageString);
         parsedString.should.include.keys('positionalArgs');
         parsedString.should.include.keys('optionalArgs');
         parsedString.should.include.keys('flags');
-    })
+    });
 });
