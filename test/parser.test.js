da9e85e0a44cf318a9822fc1b80bec9820c6fe3e
oliverhilferty <oliverhilferty@gmail.com>
2019-05-23 17:22:49 +0100

    Add basic tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,9 +1,17 @@
 const describe = require('mocha').describe;
-const chai = require('chai');
-chai.should();
+require('chai').should();
+const CommandLineHelpParser = require('../parser').CommandLineHelpParser;
+
 
 describe('Parser', function() {
-    it('Should run this test', function() {
-
+    beforeEach('Init CommandLineHelpParser', function() {
+        this.CLIParser = new CommandLineHelpParser();
     });
+
+    it('generates an object with optional arguments, positional arguments, and flags fields', function() {
+        const parsedString = this.CLIParser.parse('usage: grablio [-h] [-d DESTINATION] [-s SELECTOR] [-a ATTR] [-t] url');
+        parsedString.should.include.keys('positionalArgs');
+        parsedString.should.include.keys('optionalArgs');
+        parsedString.should.include.keys('flags');
+    })
 });
