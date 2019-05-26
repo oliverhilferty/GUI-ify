9a1527da0c5e32709c2e72aab3ba125b0e792f97
oliverhilferty <oliverhilferty@gmail.com>
2019-05-26 09:17:11 +0100

    Add flags component and container

--- js/components.js
+++ js/components.js
@@ -2,9 +2,20 @@ exports.textInput = (width, id, label, placeholder='') => {
     return `
         <div class="row">
             <div class="input-field col s${width}">
-              <input placeholder="${placeholder}" id="${label}" type="text">
-              <label for="${label}">${label}</label>
+                <input placeholder="${placeholder}" id="${label}" type="text">
+                <label for="${label}">${label}</label>
             </div>
         </div>
+    `
+};
+
+exports.checkbox = (name) => {
+    return `
+        <p>
+            <label>
+                <input type="checkbox" class="filled-in" checked="checked" />
+                <span>${name}</span>
+            </label>
+        </p>
     `
 };
