8f2337ab5e49796e65c026c40f04d7ddbc1cadd1
oliverhilferty <oliverhilferty@gmail.com>
2019-05-26 09:17:24 +0100

    Loop over flags and add to window

--- js/app.js
+++ js/app.js
@@ -11,4 +11,8 @@ electron.ipcRenderer.on('ping', (event, message) => {
     for (const arg of message.optionalArgs) {
         $('.optionalArgs').append(components.textInput(12, arg.name, arg.name));
     }
+
+    for (const flag of message.flags) {
+        $('.flags').append(components.checkbox(flag.name));
+    }
 });
